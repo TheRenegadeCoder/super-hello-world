7a2af0d9a4c45e566815ab07c4879d7af8ef1dc7
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-10-27 22:27:36 -0400

    Added Function Name Parsing (#7)
    * Added incredibly cursed file name parsing
    
    * Reworked solution to use function name

--- hello_world.py
+++ hello_world.py
@@ -1,6 +1,10 @@
-try:
-    raise Exception("Hello, World!")
-except Exception as e:
-    for character in str(e):
-        print(character, end="")
-    print()
+def hello_world():
+    error = ", ".join(map(str.capitalize, hello_world.__name__.split("_"))) + "!"
+    try:
+        raise Exception(error)
+    except Exception as e:
+        for character in str(e):
+            print(character, end="")
+        print()
+    
+hello_world()
