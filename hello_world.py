098346b71940c069c847e259eb9affc715d380fa
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-10-27 22:36:10 -0400

    Added two layers of recursion

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,29 @@
 def hello_world():
-    error = ", ".join(map(str.capitalize, hello_world.__name__.split("_"))) + "!"
+    error = format_error(get_error(hello_world.__name__))
     try:
         raise Exception(error)
     except Exception as e:
         for character in str(e):
             print(character, end="")
         print()
-    
+
+
+def format_error(error):
+    return ", ".join(map(str.capitalize, error.split("_"))) + "!"
+
+
+def get_error(message):
+    c = message[0]
+    if message[1:]:
+        c += get_exception(message[1:])
+    return c
+
+
+def get_exception(message):
+    c = message[0]
+    if message[1:]:
+        c += get_error(message[1:])
+    return c
+
+
 hello_world()
