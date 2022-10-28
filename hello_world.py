ad698c7a6fa4bb1554a9ff44475932ae7ceec8db
Jeremy Grifski <jeremy.grifski@gmail.com>
2022-10-27 22:45:48 -0400

    Added Redundant Recursion Feature (#8)
    * Added two layers of recursion
    
    * Added yet another layer of recursion
    
    * Renamed some variables
    
    * Moved the error message

--- hello_world.py
+++ hello_world.py
@@ -1,10 +1,36 @@
 def hello_world():
-    error = ", ".join(map(str.capitalize, hello_world.__name__.split("_"))) + "!"
     try:
+        error = format_error(get_error(hello_world.__name__))
         raise Exception(error)
     except Exception as e:
         for character in str(e):
             print(character, end="")
         print()
-    
+
+
+def format_error(error):
+    return ", ".join(map(str.capitalize, error.split("_"))) + "!"
+
+
+def get_error(message):
+    c = message[0]
+    if message[1:]:
+        c += get_exception(message[1:])
+    return c
+
+
+def get_exception(shout):
+    i = shout[0]
+    if shout[1:]:
+        i += get_issue(shout[1:])
+    return i
+
+
+def get_issue(whisper):
+    w = whisper[0]
+    if whisper[1:]:
+        w += get_error(whisper[1:])
+    return w
+
+
 hello_world()
